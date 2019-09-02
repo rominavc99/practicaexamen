66b221c63d896793f4dd3f0f3bd681f4800e0b2c
alumno <alumno@E308>
2019-09-02 12:38:32 -0700

    Agregar archivos de proyecto.

---
+++ ejerciciorestaurant/Ingredientes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciorestaurant
+{
+    class Ingredientes
+    {
+        public string Nombre { get; set; }
+        public string Identificador { get; set; }
+
+
+    }
+}
---
+++ ejerciciorestaurant/Platillos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciorestaurant
+{
+    class Platillos
+    {
+        public string Identificador { get; set; }
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+        private double _precio;
+        public double Precio {
+
+            get { return _precio; }
+            set
+            {
+                if (value >= 50)
+                {
+                     _precio = value;
+                }
+                else
+                {
+                    throw new Exception("es menor de 50");
+                }
+            }
+        }
+
+
+
+
+    }
+}
